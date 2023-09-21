b472fc0ce877312939a740fd4b89e3397e258065
nishanka <nishanka.rupasinghe@gmail.com>
2023-09-22 01:02:20 +1000

    Remove loading as we are doing it in a different way

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,15 +1,12 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
 
 const RootLayout = () => {
-  const navigation = useNavigation();
-
   return (
     <div>
         <MainNavigation />
         <main>
-            {navigation.state === 'loading' && <p>Loading...</p>}
             <Outlet />
         </main>
     </div>
