aa1fb87c03d547f5e9bef1ea4dd5c37bac65aded
nishanka <nishanka.rupasinghe@gmail.com>
2023-09-21 21:13:09 +1000

    Place eventLoader code inside Events.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,7 @@ import RootLayout from './pages/Root';
 import EventsRootLayout from './pages/EventsRootLayout';
 
 import HomePage from './pages/Home';
-import EventsPage from './pages/Events';
+import EventsPage, { loader as eventsLoader } from './pages/Events';
 import EventDetailPage from './pages/EventDetail';
 import NewEventPage from './pages/NewEvent';
 import EditEventPage from './pages/EditEvent';
@@ -23,17 +23,7 @@ const myRouter = createBrowserRouter([
         children: [
           { index: true, 
             element: <EventsPage />, 
-            loader: async () => {
-              const response = await fetch('http://localhost:8080/events');
-
-              if (!response.ok) {
-                // setError('Fetching events failed.');
-              } else {
-                const resData = await response.json();
-                return resData.events;
-                // setFetchedEvents(resData.events);
-              }
-            }
+            loader: eventsLoader,
           },
           { path: ':eventId', element: <EventDetailPage />},
           { path: 'new', element: <NewEventPage />},
--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -14,6 +14,18 @@ function EventsPage() {
 
 export default EventsPage;
 
+export async function loader () {
+  const response = await fetch('http://localhost:8080/events');
+
+  if (!response.ok) {
+    // setError('Fetching events failed.');
+  } else {
+    const resData = await response.json();
+    return resData.events;
+    // setFetchedEvents(resData.events);
+  }
+};
+
 // import { Link } from "react-router-dom";
 
 // const EVENTS = [
